8e8b6de6870a3ad110e0319d505cb0e091eacd4e
Russell <russellsahoo24@gmail.com>
2026-04-11 11:45:15 +0530

    added email sender

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ import { startChangeStreams } from './streams/changeStreamWatcher.js';
 import { startJobs } from './jobs/index.js';
 const logger = console;
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 5000;
 
 const server = http.createServer(app);
 
